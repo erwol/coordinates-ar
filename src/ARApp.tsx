771abddb288dd8c84c3e5b523d9205a973e2e66b
Ernesto Wulff <ernes.wo@gmail.com>
2024-11-23 20:46:29 +0100

    original model

--- src/ARApp.tsx
+++ src/ARApp.tsx
@@ -1,51 +1,69 @@
 import { Canvas } from "@react-three/fiber";
 import { XR, createXRStore } from "@react-three/xr";
-import { useRef, useState } from "react";
-import * as THREE from "three";
 
-const store = createXRStore();
+const store = createXRStore({
+  controller: false,
+  hitTest: true,
+});
 
-const TexturedPlane = () => {
-  const textureRef = useRef<THREE.CanvasTexture>();
+// const TexturedPlane = () => {
+//   const textureRef = useRef<THREE.CanvasTexture>();
 
-  // Create a canvas and draw text on it
-  const createTextTexture = () => {
-    const canvas = document.createElement("canvas") as HTMLCanvasElement;
-    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-    canvas.width = 512;
-    canvas.height = 512;
+//   // Create a canvas and draw text on it
+//   const createTextTexture = () => {
+//     const canvas = document.createElement("canvas") as HTMLCanvasElement;
+//     const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+//     canvas.width = 512;
+//     canvas.height = 512;
 
-    // Draw background
-    context.fillStyle = "black";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+//     // Draw background
+//     context.fillStyle = "black";
+//     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Draw text
-    context.fillStyle = "white";
-    context.font = "48px Arial";
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.fillText(
-      "Hello, React Three Fiber!",
-      canvas.width / 2,
-      canvas.height / 2,
-    );
+//     // Draw text
+//     context.fillStyle = "white";
+//     context.font = "48px Arial";
+//     context.textAlign = "center";
+//     context.textBaseline = "middle";
+//     context.fillText(
+//       "Hello, React Three Fiber!",
+//       canvas.width / 2,
+//       canvas.height / 2,
+//     );
 
-    return new THREE.CanvasTexture(canvas);
-  };
+//     return new THREE.CanvasTexture(canvas);
+//   };
 
-  // Assign the texture to a ref
-  if (!textureRef.current) {
-    textureRef.current = createTextTexture();
-  }
+//   // Assign the texture to a ref
+//   if (!textureRef.current) {
+//     textureRef.current = createTextTexture();
+//   }
 
-  return (
-    <mesh position={[0, 1, -5]}>
-      <boxGeometry args={[2, 3, 0.1]} />
-      {/* Material using the generated texture */}
-      <meshBasicMaterial map={textureRef.current} />
-    </mesh>
-  );
-};
+//   return (
+//     <mesh position={[0, 1, -5]}>
+//       <boxGeometry args={[2, 3, 0.1]} />
+//       {/* Material using the generated texture */}
+//       <meshBasicMaterial map={textureRef.current} />
+//     </mesh>
+//   );
+// };
+
+// const AnchoredCube = () => {
+//   const [anchor, setAnchor] = useState(null);
+//   const requestAnchor = useRequestXRAnchor();
+
+//   useEffect(() => {
+//     requestAnchor({}).then((newAnchor) => {
+//       setAnchor(newAnchor);
+//     });
+//   }, [requestAnchor]);
+
+//   return anchor ? (
+//     <Box position={[0, 1, -3]} args={[1, 1, 1]}>
+//       <meshStandardMaterial color="blue" />
+//     </Box>
+//   ) : null;
+// };
 
 // const FixedCube = () => {
 //   return (
@@ -56,21 +74,21 @@ const TexturedPlane = () => {
 // };
 
 export function ARApp() {
-  const [red, setRed] = useState(false);
+  //const [red, setRed] = useState(false);
   return (
     <>
       <button onClick={() => store.enterAR()}>Enter AR</button>
       <Canvas>
         <XR store={store}>
-          <mesh
+          {/* <mesh
             pointerEventsType={{ deny: "grab" }}
             onClick={() => setRed(!red)}
             position={[0, 1, -1]}
           >
             <boxGeometry />
             <meshBasicMaterial color={red ? "red" : "blue"} />
-          </mesh>
-          <TexturedPlane />
+          </mesh> */}
+          {/* <TexturedPlane /> */}
         </XR>
       </Canvas>
     </>
