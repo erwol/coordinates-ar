67f2cdb359c199560b75c945a2d54eb169461b66
Ernesto Wulff <ernes.wo@gmail.com>
2024-11-27 11:24:34 +0100

    feat: missing symbols

--- src/constants.ts
+++ src/constants.ts
@@ -22,8 +22,8 @@ export const AR_ITEMS: GeoARItem[] = [
   {
     id: 3,
     src: `${MEDIA_PREFIX}caidos.png`,
-    lat: 40.6413603,
-    lng: -4.1595682,
+    lat: 28.3778098,
+    lng: -16.8041513,
     name: "caidos",
   },
   {
@@ -39,5 +39,40 @@ export const AR_ITEMS: GeoARItem[] = [
     lat: 40.8138502,
     lng: 0.5200469,
     name: "ebro",
+  },
+  {
+    id: 6,
+    src: `${MEDIA_PREFIX}llano.png`,
+    lat: 35.9019722,
+    lng: -5.2964619,
+    name: "llano",
+  },
+  {
+    id: 7,
+    src: `${MEDIA_PREFIX}mola.png`,
+    lat: 42.4513837,
+    lng: -3.3607398,
+    name: "mola",
+  },
+  {
+    id: 8,
+    src: `${MEDIA_PREFIX}navarra.png`,
+    lat: 42.8090806,
+    lng: -1.6350326,
+    name: "navarra",
+  },
+  {
+    id: 9,
+    src: `${MEDIA_PREFIX}piramide.png`,
+    lat: 43.0401376,
+    lng: -3.8782522,
+    name: "piramide",
+  },
+  {
+    id: 10,
+    src: `${MEDIA_PREFIX}valle.png`,
+    lat: 40.6419984,
+    lng: -4.1506207,
+    name: "valle",
   },
 ];
